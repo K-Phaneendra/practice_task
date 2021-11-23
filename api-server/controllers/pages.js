712bab96f5c32417a726188e306470eb93d16865
K-Phaneendra <phaneendra.kosanam@hotmail.com>
2021-11-23 09:05:40 +0530

    created API to fetch pages by id

--- api-server/controllers/pages.js
+++ api-server/controllers/pages.js
@@ -6,7 +6,9 @@ var path = require('path');
 const CustomError = require("./CustomError")
 
 module.exports = class Pages {
-  constructor() {}
+  constructor(pageId) {
+    this.pageId = pageId // number
+  }
 
   // get all pages
   getAllPages() {
@@ -29,6 +31,20 @@ module.exports = class Pages {
       const parsedJson = JSON.parse(json);
       const activeRecords = parsedJson.filter(each => each.isActive);
       return activeRecords;
+    } catch (err) {
+      const error = new CustomError(err);
+      return error.body();
+    }
+  }
+
+  // get page by id
+  getPageById() {
+    try {
+      const filePath = path.join(__dirname, '..', 'data.json');
+      const json = fs.readFileSync(filePath, 'utf8');
+      const parsedJson = JSON.parse(json);
+      const pageRecord = parsedJson.find(each => each.id === this.pageId)
+      return pageRecord;
     } catch (err) {
       const error = new CustomError(err);
       return error.body();
--- api-server/index.js
+++ api-server/index.js
@@ -31,6 +31,18 @@ app.get("/active-pages", (req, res, next) => {
   }
 });
 
+app.get("/page-by-id/:id", (req, res, next) => {
+  try {
+    const pageId = Number(req.params.id);
+    const pages = new Pages(pageId);
+    const records = pages.getPageById();
+    res.status(200).send(records);
+  } catch (err) {
+    const errorBody = new CustomError(err).body();
+    res.status(400).send(errorBody);
+  }
+});
+
 app.listen(process.env.PORT, () => {
  console.log(`Server running on port ${process.env.PORT}`);
 });
